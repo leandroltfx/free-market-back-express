77e2b00ecfad6be074b0b5c68b711f19764d734b
leandroltfx <leandrobsb55@hotmail.com>
2024-03-17 09:36:39 -0300

    implementando embedding entre models user e phone

--- src/models/phone.model.js
+++ src/models/phone.model.js
@@ -1,20 +1,16 @@
 import mongoose from "mongoose";
 
-import phoneSchema from './phone.model.js';
-
 // A criação do Schema na API cria também a collection na database se ainda não existir.
 
 // Schema é um objeto de configuração que define a estrutura e as propriedades de um documento
-const userSchema = new mongoose.Schema({
+const phoneSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    phones: [phoneSchema] // Embedding
+    number: { type: String, required: true },
+    categories: { type: [String], enum: ['WhatsApp', 'Telegram', 'Celular'], default: 'Celular' }
 }, { versionKey: false });
 
 // O primeiro parâmetro se refere a collection definida (ou a ser criada) no MongoDB Atlas
 // Models são objetos que representa uma coleção na base de dados e são eles que fazem a manipulação no banco de dados na controller
-const userModel = mongoose.model("users", userSchema);
+// const phoneModel = mongoose.model("users", phoneSchema);
 
-export default userModel;
+export default phoneSchema;
